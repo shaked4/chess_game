3684ad9daba69024fde23b3cd7504007775546b3
Shaked Peller <shaked.peller@gmail.com>
2023-08-15 15:43:30 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,11 @@ import java.util.ArrayList;
 
 
 public class Main {
-    private BufferedImage bufferedImage;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 800, 830);
-        Piece[] pieces=new Piece[32];
+        Piece[] pieces = new Piece[32];
         JPanel pn = new JPanel() {
             @Override
             public void paint(Graphics g) {
@@ -32,42 +31,34 @@ public class Main {
                     }
                     white = !white;
                 }
-                BufferedImage bufferedImage = null;
+                BufferedImage bufferedImage;
                 try {
                     bufferedImage = ImageIO.read(new File("white_pawn2.png"));
-                    pieces[0]=new Pawn(0,6,Piece.COLOR.WHITE);
+                    pieces[0] = new Pawn(0, 6, Piece.COLOR.WHITE);
                     g.drawImage(bufferedImage, 10, 610, null);
 
                     bufferedImage = ImageIO.read(new File("black_knight05.png"));
-                    pieces[1]=new Knight(1,0,Piece.COLOR.BLACK);
+                    pieces[1] = new Knight(1, 0, Piece.COLOR.BLACK);
                     g.drawImage(bufferedImage, 100, 0, null);
 
                     bufferedImage = ImageIO.read(new File("white_pawn2.png"));
-                    pieces[2]=new Knight(0,4,Piece.COLOR.WHITE);
+                    pieces[2] = new Knight(0, 4, Piece.COLOR.WHITE);
                     g.drawImage(bufferedImage, 0, 410, null);
 
                     bufferedImage = ImageIO.read(new File("black_knight05.png"));
-                    pieces[3]=new Knight(1,5,Piece.COLOR.BLACK);
+                    pieces[3] = new Knight(1, 5, Piece.COLOR.BLACK);
                     g.drawImage(bufferedImage, 100, 510, null);
 
                     bufferedImage = ImageIO.read(new File("white_pawn2.png"));
-                    pieces[4]=new Pawn(5,6,Piece.COLOR.WHITE);
+                    pieces[4] = new Pawn(5, 6, Piece.COLOR.WHITE);
                     g.drawImage(bufferedImage, 510, 610, null);
 
                     bufferedImage = ImageIO.read(new File("black_knight05.png"));
-                    pieces[5]=new Knight(6,5,Piece.COLOR.BLACK);
+                    pieces[5] = new Knight(6, 5, Piece.COLOR.BLACK);
                     g.drawImage(bufferedImage, 610, 510, null);
-
-
-
-
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-
-
-
-
             }
         };
 
@@ -79,29 +70,16 @@ public class Main {
             public void mouseClicked(MouseEvent e) {
 
                 System.out.println("hello");
-                if ((e.getX()>0 && e.getX()<100) && (e.getY()>600 && e.getY()<700))
-                {
+                if ((e.getX() > 0 && e.getX() < 100) && (e.getY() > 600 && e.getY() < 700)) {
                     System.out.println("working");
                 }
-                System.out.println("cell is: "+"x:"+e.getX()/100+ " y:"+e.getY()/100);
-                int x=e.getX()/100;
-                int y=e.getY()/100;
-                ArrayList<Square> PossibleSquares=new ArrayList<>();
-                for (int i=0; i<32;i++)
-                {
-                    if (pieces[i] != null && pieces[i].square.getX()==x && pieces[i].square.getY()==y)
-                    {
-                        ///check what name, to see how he can move but for now we assume he is a white pawn
-//                        BufferedImage   bufferedImage = null;
-//                        try {
-//                            bufferedImage = ImageIO.read(new File("green_circle.png"));
-//                        } catch (IOException ex) {
-//                            throw new RuntimeException(ex);
-//                        }
-//                        Image image = bufferedImage.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-//                        pn.getGraphics().drawImage(image, x*100+20, y*100-75, null);
-//                        break;
-                        PossibleSquares=pieces[i].getPossibleSquares(pieces);
+                System.out.println("cell is: " + "x:" + e.getX() / 100 + " y:" + e.getY() / 100);
+                int x = e.getX() / 100;
+                int y = e.getY() / 100;
+                ArrayList<Square> PossibleSquares = new ArrayList<>();
+                for (int i = 0; i < 32; i++) {
+                    if (pieces[i] != null && pieces[i].square.getX() == x && pieces[i].square.getY() == y) {
+                        PossibleSquares = pieces[i].getPossibleSquares(pieces);
                         System.out.println("in on white pawn");
                         break;
                     }
@@ -109,48 +87,34 @@ public class Main {
                 if (!PossibleSquares.isEmpty()) {
 
                     System.out.println("in on possible squares");
-                    for (int i = 0; i < PossibleSquares.size(); i++)
-                    {
+                    for (int i = 0; i < PossibleSquares.size(); i++) {
                         System.out.println("in possible loop");
-                        BufferedImage   bufferedImage = null;
+                        BufferedImage bufferedImage = null;
                         try {
                             bufferedImage = ImageIO.read(new File("green_circle.png"));
                         } catch (IOException ex) {
                             throw new RuntimeException(ex);
                         }
                         Image image = bufferedImage.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
-                        System.out.println(y*100);
-                        pn.getGraphics().drawImage(image,PossibleSquares.get(i).getX()*100+20, PossibleSquares.get(i).getY()*100, null);
+                        System.out.println(y * 100);
+                        pn.getGraphics().drawImage(image, PossibleSquares.get(i).getX() * 100 + 20, PossibleSquares.get(i).getY() * 100, null);
                     }
 
                 }
             }
 
             @Override
-            public void mousePressed(MouseEvent e) {
-
-            }
+            public void mousePressed(MouseEvent e) {}
 
             @Override
-            public void mouseReleased(MouseEvent e) {
-
-
-
-            }
+            public void mouseReleased(MouseEvent e) {}
 
             @Override
-            public void mouseEntered(MouseEvent e) {
-
-            }
+            public void mouseEntered(MouseEvent e) {}
 
             @Override
-            public void mouseExited(MouseEvent e) {
-
-            }
+            public void mouseExited(MouseEvent e) {}
         });
-
-
     }
-
 }
 
