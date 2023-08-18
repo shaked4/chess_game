1e4b95c0e1843fac10e49aae38c1b6ab68ba92cc
shaked peller <shaked.peller@gmail.com>
2023-08-18 15:07:01 +0300

    adding queen getPossibleSquares

--- src/Queen.java
+++ src/Queen.java
@@ -14,7 +14,32 @@ public class Queen extends Piece {
 
     @Override
     public ArrayList<Square> getPossibleSquares(List<Piece> t) {
-        return null;
+
+
+        ArrayList<Square> options = new ArrayList<>();
+
+
+        int x = square.x;
+        int y = square.y;
+        int[][] nextMove = {{1, 0}, {0, 1}, {-1, 0}, {0, -1},{1,-1},{1,1},{-1,1},{-1,-1}};
+        for (int[] ints : nextMove) {
+            square.x = x;
+            square.y = y;
+            Square nextSquare = new Square(square.x + ints[0], square.y + ints[1]);
+            while (!isSquareTaken(nextSquare, t) && isInBoundaries(square, ints[0], ints[1])) {
+                options.add(nextSquare);
+                square.x = square.x + ints[0];
+                square.y = square.y + ints[1];
+                nextSquare = new Square(square.x + ints[0], square.y + ints[1]);
+            }
+            if (getPiece(nextSquare, t) != null && getPiece(nextSquare, t).color != this.color)
+                options.add(nextSquare);
+
+        }
+        square.x = x;
+        square.y = y;
+
+        return options;
     }
 
     @Override
